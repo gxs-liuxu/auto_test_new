397ea5b33fbc3c20e37c0c13c59a79256154886c
liuxu <liuxu@guxiansheng.cn>
2020-04-20 17:02:49 +0800

    '创建配置文件目录'

---
+++ config/config.py
@@ -0,0 +1,33 @@
+database_config = {
+    'localhost': {
+        'host': '192.168.0.96',
+        'port': 3306,
+        'user': 'root',
+        'password': '123456'
+    },
+    'gxs_lan' : {
+        'host' : '192.168.10.75',
+        'port' : 3306,
+        'user' : 'stocksir',
+        'password' : 'stocksir1704!'
+    },
+    'gxs_wan' : {
+        'host' : '192.168.10.75',
+        'port' : 3306,
+        'user' : 'stocksir',
+        'password' : 'stocksir1704!'
+    }
+}
+
+admin_info = {
+    'lx':{
+        'admin_name' : 'liuxu001',
+        'admin_id' : '10001778',
+        'appcode' : '5cc1845a44048vo1ymknj5lg',
+        'institution_id' : 1,
+        'company_id' : 2,
+        'business_id' : 689,
+        'reqorigin' : 'cloudmanager',
+        'key' : '5e13f39c3e10egormjbz0xwo'
+    }
+}
