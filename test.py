e6a9d60fbd36032b902ce48bf68ac3cc8c04bcaf
liuxu <liuxu@guxiansheng.cn>
2020-04-21 15:54:32 +0800

    process_list添加个人中心

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from public.interface.process_class import process_class
 
 def main():
     a = process_class()
-    a.set_test_process(['登录']) #list内使用单引号隔开，不要使用双引号
+    a.set_test_process(['登录','个人中心']) #list内使用单引号隔开，不要使用双引号
     a.set_test_project("股先生APP")
     a.set_test_task("日常巡检")
     a.run()
