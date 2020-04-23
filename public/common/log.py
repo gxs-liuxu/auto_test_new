1bbf2af26589b57d02c8ae3bd0bd05ce7d88c950
unknown <15183691117@163.com>
2020-04-23 17:44:41 +0800

    处理Pycharm查看error级别的log文件出现编码异常问题

--- public/common/log.py
+++ public/common/log.py
@@ -21,7 +21,7 @@ logging.getLogger('').addHandler(console)
 
 #报错的日志
 #定义一个RotatingFileHandler，最多备份5个日志文件，每个日志文件最大10M
-Rthandler = RotatingFileHandler('./log/error.log', maxBytes=10*1024*1024,backupCount=5)
+Rthandler = RotatingFileHandler('./log/error.log', maxBytes=10*1024*1024,backupCount=5, encoding='utf-8')
 Rthandler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
 Rthandler.setFormatter(formatter)
